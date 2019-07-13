7a0860eb24c902492db4df3affe19f83e3931caa
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-07-13 23:26:39 +0530

    First React App

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+    render() {
+        //return React.createElement("h1",null,"hello React.js!!!!");
+        //use jsx here for simplycity
+        return <h1>Hello react.js es6!!!!</h1>;
+    }
+}
+
+//let e1=<h1>hello react.js!!!!</h1>
+//let el=React.createElement("h1",null,"hello React.js!!!!");
+
+export default App;
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+//using jsx
+//let el=React.createElement("h1",null,"hello React.js!!!!");
+//let el=<h1>hello React.js!!!!</h1>
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+//serviceWorker.unregister();
