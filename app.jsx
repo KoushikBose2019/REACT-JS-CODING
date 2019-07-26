88e043db52aa1cd2a1a709fcb215bb386e06fbff
Koushik Bose <48059128+KoushikBose2019@users.noreply.github.com>
2019-07-27 01:23:08 +0530

    Routing in React.js

--- app.jsx
+++ app.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
 
-class App extends React.Component {
-   render() {
-      return (
-         <div>
-            <Header/>
-            <Content/>
-         </div>
-      );
-   }
-}
-class Header extends React.Component {
-   render() {
-      return (
-         <div>
-            <h1>Header</h1>
-         </div>
-      );
-   }
-}
-class Content extends React.Component {
-   render() {
-      return (
-         <div>
-            <h2>Content</h2>
-            <p>The content text!!!</p>
-         </div>
-      );
-   }
+class App extends Component {
+  render() {
+    return (
+    <Router>
+        <div>
+          <h2>Welcome to React Router Tutorial</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li><Link to={'/'} className="nav-link"> Home </Link></li>
+            <li><Link to={'/contact'} className="nav-link">Contact</Link></li>
+            <li><Link to={'/about'} className="nav-link">About</Link></li>
+          </ul>
+          </nav>
+          <hr />
+          <Switch>
+              <Route exact path='/' component={Home} />
+              <Route path='/contact' component={Contact} />
+              <Route path='/about' component={About} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 export default App;
